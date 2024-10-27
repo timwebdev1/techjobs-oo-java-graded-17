7b4c33071877f7f097202ec90017ae07521024cd
timwebdev1 <tlmcdermott79@gmail.com>
2024-10-26 20:03:53 -0500

    created class tests, 2 tests pass

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,36 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.testng.Assert.*;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        final Job job1 = new Job();
+        final Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId(), "Jobs are not equal.");
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence") );
+
+
+        assertTrue(job3 instanceof Job);
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertTrue(job3.getLocation() instanceof Location);
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals(String.valueOf(job3.getName()), "Product tester", "name.value generator test.");
+        assertEquals(String.valueOf(job3.getEmployer()),"ACME","Employer.value generator test.");
+        assertEquals(String.valueOf(job3.getLocation()), "Desert", "Location.value generator test.");
+        assertEquals(String.valueOf(job3.getPositionType()), "Quality control", "PositionType.value generator test.");
+        assertEquals(String.valueOf(job3.getCoreCompetency()), "Persistence", "CoreCompetency.value generator test.");
+    }
+
 }
